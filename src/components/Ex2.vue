c2d10f9ece4e80923d7803f477676ba96c1a1b3c
Gordon Lim <limgordon@hotmail.com>
2025-10-21 04:57:23 +0000

    Ex2 Complete

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,24 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem : "",
             }
             
+        },
+        computed: {},
+        methods : {
+            deleteItem(item) {
+                const index = this.items.indexOf(item);
+                if (index > -1) { // only splice array when item is found
+                    this.items.splice(index, 1); // 2nd parameter means remove one item only
+                }
+            },
+            addItem(item) {
+                if (item !== "") {
+                    this.items.push(item);
+                }
+            }
         }
     }
 </script>
@@ -14,5 +29,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for="item in items">{{item}}
+            <strong @click="deleteItem(item)"> Delete! </strong>
+        </li>
+    </ul>
+
+    <input v-model="newItem"></input>
+    <button @click="addItem(newItem)">Add!</button>
    
 </template>
